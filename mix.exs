50b96a1986e415156faf79c1e7f02733811735a3
Zack Siri <zacksiri@gmail.com>
2020-05-01 23:30:18 +0700

    Added hexdocs

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Lexdee.MixProject do
       {:tesla, "~> 1.3.0"},
       {:jason, ">= 1.0.0"},
       {:mint, "~> 1.0"},
-      {:x509, "~> 0.8.1"}
+      {:x509, "~> 0.8.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
