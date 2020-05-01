52bf6ec23e955a3c85a7882a2357d9513c567de3
Zack Siri <zacksiri@gmail.com>
2020-05-01 16:24:57 +0700

    Added start state

--- lib/lexdee/instances/state.ex
+++ lib/lexdee/instances/state.ex
@@ -1,14 +1,19 @@
 defmodule Lexdee.Instances.State do
   @path "/state"
 
-  def start(client, id, options) do
-    params = %{
-      "action" => "start",
+  def start(client, id, options),
+    do:
+      Tesla.put(
+        client,
+        Path.join(id, @path),
+        build_params(%{"action" => "start"}, options)
+      )
+
+  defp build_params(base, options) do
+    Map.merge(base, %{
       "timeout" => Keyword.get(options, :timeout, 30),
       "force" => Keyword.get(options, :force, false),
       "stateful" => Keyword.get(options, :stateful, false)
-    }
-
-    Tesla.put(client, Path.join(id, @path), params)
+    })
   end
 end
