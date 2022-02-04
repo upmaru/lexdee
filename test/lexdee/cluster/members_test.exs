dfe32ba613c8caa60adf56af8b2e5dffd81d1f71
Zack Siri <zacksiri@gmail.com>
2022-02-04 08:42:35 +0700

    add test for list cluster members

--- test/lexdee/cluster/members_test.exs
+++ test/lexdee/cluster/members_test.exs
@@ -1,6 +1,37 @@
 defmodule Lexdee.Cluster.MembersTest do
   use ExUnit.Case
 
+  describe "index recursive 1" do
+    setup do
+      response =
+        File.read!(
+          "test/support/fixtures/responses/cluster/member/index_recursive_one.json"
+        )
+
+      bypass = Bypass.open()
+      client = Lexdee.create_client("http://localhost:#{bypass.port}")
+
+      {:ok, client: client, bypass: bypass, response: response}
+    end
+
+    test "return success list cluster members", %{
+      bypass: bypass,
+      client: client,
+      response: response
+    } do
+      Bypass.expect(bypass, "GET", "/1.0/cluster/members", fn conn ->
+        %{"recursion" => "1"} = conn.query_params
+
+        conn
+        |> Plug.Conn.put_resp_header("content-type", "application/json")
+        |> Plug.Conn.resp(200, response)
+      end)
+
+      assert {:ok, _data} =
+               Lexdee.list_cluster_members(client, query: [recursion: 1])
+    end
+  end
+
   describe "show member" do
     setup do
       response =
