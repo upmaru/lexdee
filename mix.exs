341b5b23e5fe67ab9577158548273382c3dba547
Zack Siri <me@zacksiri.dev>
2023-03-06 13:47:24 +0700

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lexdee.MixProject do
   def project do
     [
       app: :lexdee,
-      version: "2.1.1",
+      version: "2.2.0",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
