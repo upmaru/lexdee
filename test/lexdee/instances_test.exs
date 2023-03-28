23077df938746bd6dd0d9d7325a461f336cf07dc
Zack Siri <me@zacksiri.dev>
2023-03-28 21:36:42 +0700

    Add test and support for project on get_instance
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- test/lexdee/instances_test.exs
+++ test/lexdee/instances_test.exs
@@ -9,6 +9,39 @@ defmodule Lexdee.InstancesTest do
     {:ok, client: client, bypass: bypass}
   end
 
+  describe "get instance" do
+    setup do
+      response =
+        File.read!("test/support/fixtures/responses/instances/show.json")
+
+      {:ok, response: response}
+    end
+
+    test "return success for get instance with project", %{
+      bypass: bypass,
+      client: client,
+      response: response
+    } do
+      Bypass.expect(
+        bypass,
+        "GET",
+        "/1.0/instances/instellar-staging-01",
+        fn conn ->
+          assert conn.query_string == "project=something"
+
+          conn
+          |> Plug.Conn.put_resp_header("content-type", "application/json")
+          |> Plug.Conn.resp(202, response)
+        end
+      )
+
+      assert {:ok, _data} =
+               Lexdee.get_instance(client, "instellar-staging-01",
+                 query: [project: "something"]
+               )
+    end
+  end
+
   describe "create instance" do
     setup do
       response =
