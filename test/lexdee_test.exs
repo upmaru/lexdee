b728ce55669b8e53a22fe0495e60fd26894bd274
Zack Siri <me@zacksiri.dev>
2024-04-12 11:35:51 +0700

    Add test to allow passing timeout options

--- test/lexdee_test.exs
+++ test/lexdee_test.exs
@@ -1,3 +1,17 @@
 defmodule LexdeeTest do
   use ExUnit.Case
+
+  test "create client" do
+    assert %Tesla.Client{adapter: adapter} =
+             Lexdee.create_client(
+               "http://localhost:8443",
+               "somecert",
+               "somekey",
+               timeout: 300_000
+             )
+
+    assert {Tesla.Adapter.Mint, :call, [options]} = adapter
+
+    assert Keyword.fetch!(options, :timeout) == 300_000
+  end
 end
